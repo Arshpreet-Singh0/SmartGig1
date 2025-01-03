d24a9af2b0c76adcbac41f2753518bc6acfcf1fb
Arshpreet Singh <arshsomal100@gmail.com>
2025-01-03 15:21:08 +0530

    Added redux-persit

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,12 +1,42 @@
-import { configureStore } from '@reduxjs/toolkit'
-import authSlice from './authSlice'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
+import authSlice from './authSlice';
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 // ...
 
-export const store = configureStore({
-  reducer: {
-    auth : authSlice
-  }
-})
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+
+const rootReducer = combineReducers({
+  auth: authSlice,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export default store;
 
 // Infer the `RootState`,  `AppDispatch`, and `AppStore` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
