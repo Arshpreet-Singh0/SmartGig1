2061bc69a9dc61b35ff0635cad937d7195845053
Arshpreet Singh <arshsomal100@gmail.com>
2025-01-13 20:11:10 +0530

    Added side nav for admin

--- frontend/src/components/navbar/Sidebar.tsx
+++ frontend/src/components/navbar/Sidebar.tsx
@@ -1,35 +1,136 @@
-import { FileText, Menu, MessageSquareMore, WalletCards } from "lucide-react"
+import {
+  FileText,
+  Home,
+  Menu,
+  MessageSquareMore,
+  Plus,
+  User,
+  WalletCards,
+} from "lucide-react";
 import { useState } from "react";
+import { useAppSelector } from "../../hooks/hook";
 
 const Sidebar = () => {
-    const [selected, setSlected] = useState(1);
+  const [selected, setSlected] = useState(1);
+  const { user } = useAppSelector((store) => store.auth);
   return (
     <div className="flex flex-col gap-4 bg-[#262626] w-64 h-screen sticky top-0 z-10 text-[#DBDBDB] px-6 border-r border-gray-700">
-        <div className="flex items-center h-16">
-            <h1 className="text-3xl font-semibold">Smart Gig</h1>
-        </div>
-
-        <div className={`flex items-center px-3 py-[0.75rem] rounded-lg ${selected==1 ? 'bg-[#404040]' : ''} hover:bg-[#404040]`} onClick={()=>setSlected(1)}>
-            <Menu className="mr-4" width={22}/>
+      <div className="flex items-center h-16">
+        <h1 className="text-3xl font-semibold">Smart Gig</h1>
+      </div>
+      {user.accountType == "FREELANCER" ? (
+        <>
+          <div
+            className={`flex items-center px-3 py-[0.75rem] rounded-lg ${
+              selected == 1 ? "bg-[#404040]" : ""
+            } hover:bg-[#404040]`}
+            onClick={() => setSlected(1)}
+          >
+            <Menu className="mr-4" width={22} />
             <p className="text-sm">DashBoard</p>
-        </div>
-        <a href={'#projects'} className="text-sm">
-        <div className={`flex items-center px-3 py-[0.75rem] rounded-lg ${selected==2 ? 'bg-[#404040]' : ''} hover:bg-[#404040]`} onClick={()=>setSlected(2)}>
-            <WalletCards className="mr-4" width={22}/>
-            Projects
-        </div></a>
-        <div className={`flex items-center px-3 py-[0.75rem] rounded-lg ${selected==3 ? 'bg-[#404040]' : ''} hover:bg-[#404040]`} onClick={()=>setSlected(3)}>
-            <MessageSquareMore className="mr-4" width={22}/>
+          </div>
+          <a href={"#projects"} className="text-sm">
+            <div
+              className={`flex items-center px-3 py-[0.75rem] rounded-lg ${
+                selected == 2 ? "bg-[#404040]" : ""
+              } hover:bg-[#404040]`}
+              onClick={() => setSlected(2)}
+            >
+              <WalletCards className="mr-4" width={22} />
+              Projects
+            </div>
+          </a>
+          <div
+            className={`flex items-center px-3 py-[0.75rem] rounded-lg ${
+              selected == 3 ? "bg-[#404040]" : ""
+            } hover:bg-[#404040]`}
+            onClick={() => setSlected(3)}
+          >
+            <MessageSquareMore className="mr-4" width={22} />
             <p className="text-sm">Messages</p>
-        </div>
-        <a href="#Proposals" className="text-sm">
-        <div className={`flex items-center px-3 py-[0.75rem] rounded-lg ${selected==4 ? 'bg-[#404040]' : ''} hover:bg-[#404040]`} onClick={()=>setSlected(4)}>
-            <FileText className="mr-4" width={22}/>
-            Proposals
-            {/* <p className="text-sm"></p> */}
-        </div></a>
+          </div>
+          <a href="#Proposals" className="text-sm">
+            <div
+              className={`flex items-center px-3 py-[0.75rem] rounded-lg ${
+                selected == 4 ? "bg-[#404040]" : ""
+              } hover:bg-[#404040]`}
+              onClick={() => setSlected(4)}
+            >
+              <FileText className="mr-4" width={22} />
+              Proposals
+              {/* <p className="text-sm"></p> */}
+            </div>
+          </a>
+        </>
+      ) : (
+        <>
+          <div
+            className={`flex items-center px-3 py-[0.75rem] rounded-lg ${
+              selected == 1 ? "bg-[#404040]" : ""
+            } hover:bg-[#404040]`}
+            onClick={() => setSlected(1)}
+          >
+            <Home className="mr-4" width={22} />
+            <p className="text-sm">DashBoard</p>
+          </div>
+          <a href={"#projects"} className="text-sm">
+            <div
+              className={`flex items-center px-3 py-[0.75rem] rounded-lg ${
+                selected == 2 ? "bg-[#404040]" : ""
+              } hover:bg-[#404040]`}
+              onClick={() => setSlected(2)}
+            >
+              <FileText className="mr-4" width={22} />
+              Projects
+            </div>
+          </a>
+          <a href={"#projects"} className="text-sm">
+            <div
+              className={`flex items-center px-3 py-[0.75rem] rounded-lg ${
+                selected == 3 ? "bg-[#404040]" : ""
+              } hover:bg-[#404040]`}
+              onClick={() => setSlected(3)}
+            >
+              <Plus className="mr-4" width={22} />
+              Post
+            </div>
+          </a>
+          <a href={"#projects"} className="text-sm">
+            <div
+              className={`flex items-center px-3 py-[0.75rem] rounded-lg ${
+                selected == 4 ? "bg-[#404040]" : ""
+              } hover:bg-[#404040]`}
+              onClick={() => setSlected(4)}
+            >
+              <User className="mr-4" width={22} />
+              Hire
+            </div>
+          </a>
+          <div
+            className={`flex items-center px-3 py-[0.75rem] rounded-lg ${
+              selected == 4 ? "bg-[#404040]" : ""
+            } hover:bg-[#404040]`}
+            onClick={() => setSlected(5)}
+          >
+            <MessageSquareMore className="mr-4" width={22} />
+            <p className="text-sm">Messages</p>
+          </div>
+          <a href="#Proposals" className="text-sm">
+            <div
+              className={`flex items-center px-3 py-[0.75rem] rounded-lg ${
+                selected == 5 ? "bg-[#404040]" : ""
+              } hover:bg-[#404040]`}
+              onClick={() => setSlected(6)}
+            >
+              <FileText className="mr-4" width={22} />
+              Payments
+              {/* <p className="text-sm"></p> */}
+            </div>
+          </a>
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
--- frontend/src/components/userDashboard/ui/Nav.tsx
+++ frontend/src/components/userDashboard/ui/Nav.tsx
@@ -1,7 +1,16 @@
 import { Bell } from "lucide-react"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import { clearUser } from '../../../redux/authSlice' 
+import { useDispatch } from "react-redux";
 
 const Nav = () => {
+  const dispatch = useDispatch();
+  
+  const navigate = useNavigate();
+  const logout = ()=>{
+      dispatch(clearUser());
+      navigate('/signin?next=/dashboard');
+  }
   return (
     <div className="flex items-center justify-between h-20 bg-black-200 w-full px-6 text-white sticky top-0 border-b border-gray-700">
         <div>
@@ -10,6 +19,7 @@ const Nav = () => {
         <div className="flex justify-center items-center gap-10">
             <Link to={'/projects'} >All Projects</Link>
             <Bell className="opacity-60"/>
+            <p className="cursor-pointer" onClick={logout}>Logout</p>
 
             <div className="h-10 w-10 rounded-full">
                     <img src="https://avatar.iran.liara.run/public" alt="" />
